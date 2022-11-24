65bf5d600500295949647e0d4f6732ec8e736596
L0rd0fPyth0n <117177381+L0rd0fPyth0n@users.noreply.github.com>
2022-11-24 01:41:17 +0000

    Update RequestsCreators.py

--- RequestsCreators.py
+++ RequestsCreators.py
@@ -1,6 +1,8 @@
 import time as t
 """tony, gera um temp aleatorio,
 poe isto a dormir este tempo
-(com t.sleep(), provavelmente está em ms)
-e depois põe uma pessoa a pedir á maq 1 café, repete isto em while(true)
+(com t.sleep(), provavelmente está em ms,
+vé o code example q te deixei)
+e depois põe uma pessoa a pedir 
+á maq 1 café, repete isto em while(true)
 ou até desligarmos o programa"""

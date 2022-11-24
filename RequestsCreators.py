d22be2388b41292f97f9ff27a19e7cf56333a452
L0rd0fPyth0n <117177381+L0rd0fPyth0n@users.noreply.github.com>
2022-11-24 01:28:41 +0000

    Add files via upload

---
+++ RequestsCreators.py
@@ -0,0 +1,6 @@
+import time as t
+"""tony, gera um temp aleatorio,
+poe isto a dormir este tempo
+(com t.sleep(), provavelmente está em ms)
+e depois põe uma pessoa a pedir á maq 1 café, repete isto em while(true)
+ou até desligarmos o programa"""

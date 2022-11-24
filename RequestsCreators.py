ddd7ed96176a4caa9717e23aa5aa30c39b016efa
L0rd0fPyth0n <117177381+L0rd0fPyth0n@users.noreply.github.com>
2022-11-24 20:33:42 +0000

    Update RequestsCreators.py

--- RequestsCreators.py
+++ RequestsCreators.py
@@ -6,3 +6,13 @@ vé o code example q te deixei)
 e depois põe uma pessoa a pedir 
 á maq 1 café, repete isto em while(true)
 ou até desligarmos o programa"""
+
+class Order:
+  def __init__(self, coffeType, sugarAmount, waterAmount, howHot):
+    self.coffeType = coffeType
+    self.sugarAmount = sugarAmount
+    self.waterAmount = waterAmount
+    self.howHot = howHot
+  
+  def consume():
+

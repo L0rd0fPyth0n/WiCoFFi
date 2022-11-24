e23c7edd788116a25694db1112fae87c53e93382
L0rd0fPyth0n <117177381+L0rd0fPyth0n@users.noreply.github.com>
2022-11-24 20:15:10 +0000

    Update WiCoFFISofware.py

--- WiCoFFISofware.py
+++ WiCoFFISofware.py
@@ -1,1 +1,13 @@
+# usar queues
 
+from Queue import Queue
+
+BufferSize = 6
+
+memBuffer = Queue(maxsize = BufferSize)
+waitingTime = 1
+
+
+while(True):
+    request = memBuffer.get(block=True, timeout=None) #bloqueia a thread ate haver pedidos de cafe
+        

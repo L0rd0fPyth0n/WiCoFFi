1ea078d908b3f937d73279e38d861a420722eb05
L0rd0fPyth0n <117177381+L0rd0fPyth0n@users.noreply.github.com>
2022-11-27 17:59:54 +0000

    Update WiCoFFISofware.py

--- WiCoFFISofware.py
+++ WiCoFFISofware.py
@@ -1,13 +1,30 @@
 # usar queues
-
 from Queue import Queue
+import numpy as np
+
+
 
 BufferSize = 6
-
 memBuffer = Queue(maxsize = BufferSize)
 waitingTime = 1
 
+waterTankFullCapacity = 500 #ml
+waterTankCurrCapacity = tankFullCapacity
+
+sugarTankFullCapacity = 500 #ml
+sugarTankCurrCapacity = tankFullCapacity
+
+
+
+
+#pourVel = 
+
+
 
 while(True):
-    request = memBuffer.get(block=True, timeout=None) #bloqueia a thread ate haver pedidos de cafe
-        
+    request = memBuffer.get(block=True, timeout=None)
+    #bloqueia a thread ate haver pedidos de cafe
+    waterTankCurrCapacity = waterTankCurrCapacity - request.water
+    sugarTankCurrCapacity = sugarTankCurrCapacity - request.sugar
+    #sleep()  vai depender de quê? implementa o tempo da máquina fazer o café
+    print("An" + request.coffeType + "with " + request.sugar + " gr of sugar and " + request.water + " mls of water" )

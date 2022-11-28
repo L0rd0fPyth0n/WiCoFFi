f8de5b1ec58822a8d819f8fa5a294e195988080a
L0rd0fPyth0n <117177381+L0rd0fPyth0n@users.noreply.github.com>
2022-11-28 14:25:36 +0000

    Update WiCoFFISofware.py

--- WiCoFFISofware.py
+++ WiCoFFISofware.py
@@ -11,20 +11,22 @@ waitingTime = 1
 waterTankFullCapacity = 500 #ml
 waterTankCurrCapacity = tankFullCapacity
 
-sugarTankFullCapacity = 500 #ml
+sugarTankFullCapacity = 20 #g
 sugarTankCurrCapacity = tankFullCapacity
 
 
 
 
-#pourVel = 
+#pourVel =   #in ml/s #equal for coffe, milk and all other ingredients
 
 
 
 while(True):
     request = memBuffer.get(block=True, timeout=None)
-    #bloqueia a thread ate haver pedidos de cafe
+    #bloqueia a thread (isto) ate haver pedidos de cafe
     waterTankCurrCapacity = waterTankCurrCapacity - request.water
     sugarTankCurrCapacity = sugarTankCurrCapacity - request.sugar
     #sleep()  vai depender de quê? implementa o tempo da máquina fazer o café
+    
+    #TimeOfPour = request.water/pourVel  #time to fill the cup with the requested amount of water
     print("An" + request.coffeType + "with " + request.sugar + " gr of sugar and " + request.water + " mls of water" )

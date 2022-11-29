a343bac10b279ae4b0c6b1d60d798962b665447a
L0rd0fPyth0n <117177381+L0rd0fPyth0n@users.noreply.github.com>
2022-11-29 13:18:30 +0000

    Update RequestsCreators.py

--- RequestsCreators.py
+++ RequestsCreators.py
@@ -1,4 +1,6 @@
 import time as t
+from Queue import Queue
+
 """tony, gera um temp aleatorio,
 poe isto a dormir este tempo
 (com t.sleep(), provavelmente está em ms,
@@ -6,6 +8,10 @@ vé o code example q te deixei)
 e depois põe uma pessoa a pedir 
 á maq 1 café, repete isto em while(true)
 ou até desligarmos o programa"""
+
+BufferSize = 6
+memBuffer = Queue(maxsize = BufferSize)
+
 
 class Order:
   def __init__(self, coffeType, sugar, water, howHot):
@@ -18,4 +24,12 @@ class Order:
     return self.coffeType + "with " + self.sugar + " gr of sugar and " + self.water + " mls of water"
   #to keep working in
   #def consume():
+  
+#def askCoffe():
+  if(memBuffer.full()):
+    println("Memory buffer full, ask again later")
+    # decidir em grupo como fazemos
+  else
+
+#def makeNextCoffe(): 
 

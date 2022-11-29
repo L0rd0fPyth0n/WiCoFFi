cccedb9c9542bd99774b2ac727545f3df492974c
L0rd0fPyth0n <117177381+L0rd0fPyth0n@users.noreply.github.com>
2022-11-29 12:03:31 +0000

    Update histograms.py

--- histograms.py
+++ histograms.py
@@ -1,35 +1,31 @@
 import random as r
 import matplotlib.pyplot as plt
+import numpy as np
 def getSugarAmount():
-    r.choice([1, 2, 3, 4, 5])
+    return r.choice([1, 2, 3, 4, 5])
 
 i = 0
 
-countOf0s = 0
-countOf1s = 0
-countOf2s = 0
-countOf3s = 0
-countOf4s = 0
+arr = [0,0,0,0,0]
 
-while(i < 10000):
+while(i < 100000):
     n =  getSugarAmount()
-    if(n == 0):
-        countOf0s = countOf0s + 1
-    elif (n == 1):
-        countOf1s = countOf1s + 1
-    elif (n == 2):
-        countOf2s = countOf2s + 1
-    elif (n == 3):
-        countOf3s = countOf3s + 1
-    elif (n == 4):
-        countOf4s = countOf4s + 1
+    if(n -1 == 0):
+        arr[n -1] = arr[n -1] + 1
+    elif (n -1 == 1):
+        arr[n -1] = arr[n -1] + 1
+    elif (n -1 == 2):
+        arr[n -1] = arr[n -1] + 1
+    elif (n -1 == 3):
+        arr[n -1] = arr[n -1] + 1
+    elif (n -1 == 4):
+        arr[n -1] = arr[n -1] + 1
     i = i + 1
 
 plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.xlim(0, 5)  
-plt.ylim(0, max(counts) + 1000)
-plt.plot(0,countOf0s/10000)
-#plt.plot(0,countOf0s)
-#plt.plot(0,countOf0s)
-plt.show()
+plt.ylim(0, max(arr) + 1000)
+plt.plot(range(0,4),arr)
+
+plt.show(

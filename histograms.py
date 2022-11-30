36e74f57481819edd3e764e1d5ccd94c923d0e01
L0rd0fPyth0n <117177381+L0rd0fPyth0n@users.noreply.github.com>
2022-11-30 12:14:57 +0000

    Update histograms.py

--- histograms.py
+++ histograms.py
@@ -2,7 +2,7 @@ import random as r
 import matplotlib.pyplot as plt
 import numpy as np
 def getSugarAmount():
-    return r.choice([1, 2, 3, 4, 5])
+    return r.choice(range(5))
 
 i = 0
 
@@ -10,22 +10,13 @@ arr = [0,0,0,0,0]
 
 while(i < 100000):
     n =  getSugarAmount()
-    if(n -1 == 0):
-        arr[n -1] = arr[n -1] + 1
-    elif (n -1 == 1):
-        arr[n -1] = arr[n -1] + 1
-    elif (n -1 == 2):
-        arr[n -1] = arr[n -1] + 1
-    elif (n -1 == 3):
-        arr[n -1] = arr[n -1] + 1
-    elif (n -1 == 4):
-        arr[n -1] = arr[n -1] + 1
+    arr[n ] = arr[n ] + 1
     i = i + 1
 
 plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.xlim(0, 5)  
 plt.ylim(0, max(arr) + 1000)
-plt.plot(range(0,4),arr)
+plt.plot(range(5),arr)
 
 plt.show()

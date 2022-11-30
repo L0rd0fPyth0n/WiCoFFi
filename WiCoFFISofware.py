8b9b31948f02060fdfb41cff947a9562ecebe218
L0rd0fPyth0n <117177381+L0rd0fPyth0n@users.noreply.github.com>
2022-11-30 15:05:10 +0000

    Update WiCoFFISofware.py

--- WiCoFFISofware.py
+++ WiCoFFISofware.py
@@ -5,7 +5,10 @@ import numpy as np
 
 BufferSize = 6
 memBuffer = Queue(maxsize = BufferSize)
-waitingTime = 1
+
+heatProduced = 4 #watt/s
+
+tankWaterTemp = 17
 
 waterTankFullCapacity = 500 #ml
 waterTankCurrCapacity = tankFullCapacity
@@ -16,15 +19,31 @@ sugarTankCurrCapacity = tankFullCapacity
 #Chocolate, espresso e leite quente
 pourVel = 2.5 #in ml/s #equal for coffe, milk and all other ingredients
 
-while(True):
-    request = memBuffer.get(block=True, timeout=None) #bloqueia a thread (isto) ate haver pedidos de cafe
+def seeIfCanBeMade():
     #view Tanks, e se tem outros ingredientes, se n esperar x tempo para encher
-    print("Preparing" + request)
+    if(waterTankCurrCapacity - request.water < 0):
+        sleep()
+    if(sugarTankCurrCapacity - request.sugar < 0):
+        sleep()
+
+def takingTheIngredients():
     waterTankCurrCapacity = waterTankCurrCapacity - request.water
     sugarTankCurrCapacity = sugarTankCurrCapacity - request.sugar
     
-    #TimeOfPour = request.water/pourVel  # base on time = Distance/vel  #time to fill the cup with the requested amount of water
-    #sleep()  vai depender de quê? implementa o tempo da máquina fazer o café
+
+while(True):
+    request = memBuffer.get(block=True, timeout=None) #bloqueia a thread (isto) ate haver pedidos de cafe
+    
+    seeIfCanBeMade()
+
+    
+    print("Preparing" + request)
+    
+    takingTheIngredients()
+    TempDiff = request.howHot - tankWaterTemp
+    
+    TimeOfPour = request.water/pourVel  # base on time = Distance/vel  #time to fill the cup with the requested amount of water
+    sleep(TimeOfPour + m.log(TempDiff,heatProduced))  vai depender de quê? implementa o tempo da máquina fazer o café
     
     print("An" + request + " ready")
     #waits for person to remove

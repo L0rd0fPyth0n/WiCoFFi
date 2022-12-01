33156617ab1af71d9701fd72f132dfc03685187a
L0rd0fPyth0n <117177381+L0rd0fPyth0n@users.noreply.github.com>
2022-12-01 22:28:58 +0000

    Update WiCoFFISofware.py

--- WiCoFFISofware.py
+++ WiCoFFISofware.py
@@ -6,6 +6,7 @@ import numpy as np
 BufferSize = 6
 memBuffer = Queue(maxsize = BufferSize)
 
+MaxWaitTime = 1.5*60 #in s
 heatProduced = 4 #watt/s
 
 tankWaterTemp = 17
@@ -16,17 +17,14 @@ waterTankCurrCapacity = tankFullCapacity
 sugarTankFullCapacity = 20 #g
 sugarTankCurrCapacity = tankFullCapacity
 
-#Chocolate, espresso e leite quente
 pourVel = 2.5 #in ml/s #equal for coffe, milk and all other ingredients
 
-def seeIfCanBeMade():
-    #view Tanks, e se tem outros ingredientes, se n esperar x tempo para encher
-    if(waterTankCurrCapacity - request.water < 0):
-        sleep()
-    if(sugarTankCurrCapacity - request.sugar < 0):
-        sleep()
+def canBeMade(request):
+    #view Tanks
+    return (waterTankCurrCapacity - request.water < 0) and (sugarTankCurrCapacity - request.sugar < 0)
+    
 
-def takingTheIngredients():
+def takingTheIngredients(request):
     waterTankCurrCapacity = waterTankCurrCapacity - request.water
     sugarTankCurrCapacity = sugarTankCurrCapacity - request.sugar
     
@@ -34,18 +32,19 @@ def takingTheIngredients():
 while(True):
     request = memBuffer.get(block=True, timeout=None) #bloqueia a thread (isto) ate haver pedidos de cafe
     
-    seeIfCanBeMade()
-
+    if not CanBeMade(request):
+        sleep()
+        waterTankCurrCapacity = tankFullCapacity
+        sugarTankCurrCapacity = tankFullCapacity
+        
+    print("Preparing " + request + "\n")
     
+    takingTheIngredients(request)
     
-    print("Preparing" + request)
-    
-    takingTheIngredients()
     TempDiff = request.howHot - tankWaterTemp
+    timeOfPour = request.water/pourVel  # base on time = Distance/vel  #time to fill the cup with the requested amount of water
+    heatingTime = m.log(TempDiff,heatProduced) 
+    sleep(timeOfPour + heatingTime)
     
-    TimeOfPour = request.water/pourVel  # base on time = Distance/vel  #time to fill the cup with the requested amount of water
-    sleep(TimeOfPour + m.log(TempDiff,heatProduced))  vai depender de quê? implementa o tempo da máquina fazer o café
-    
-    print("An" + request + " ready")
-    #waits for person to remove
-    
+    print("An" + request + " ready!\nYou have " + MaxWaitTime + " to get it!\n")
+    sleep(MaxWaitTime)    
